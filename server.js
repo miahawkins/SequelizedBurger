f46875905f88670c39b6d36595524604f72fe4c6
Mia Hawkins <ilearnarcheology@aol.com>
2018-01-13 09:52:18 -0600

    change server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
-// Import Routes
-var routes = require("./controllers/burgers_controller.js");
+
 
 // Start express
 var port = process.env.PORT || 3000;
@@ -21,6 +20,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Make CSS work
 // app.use(express.static(process.cwd() + "/public"));
 app.use(express.static('public'));
+
+// Import Routes
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
