392a80b5c7af1bb88c2865d319f49308d7cc4a85
Mia Hawkins <ilearnarcheology@aol.com>
2018-01-13 09:58:18 -0600

    server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Required dependencies
 var express = require("express");
-var methodOverride = require("method-override");
+// var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
 
@@ -9,21 +9,24 @@ var bodyParser = require("body-parser");
 var port = process.env.PORT || 3000;
 console.log(port);
 var app = express();
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Set handlebars as the view engine
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(bodyParser.urlencoded({extended: false}));
+
 
 // Make CSS work
 // app.use(express.static(process.cwd() + "/public"));
-app.use(express.static('public'));
+
 
 // Import Routes
 var routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+app.use(routes);
 
 app.listen(port);
